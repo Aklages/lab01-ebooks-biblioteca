91c5991e74dcd5a1acf6b6fe396655991fd3ec23
Aklages <ak1lages@gmail.com>
2026-08-23 18:05:41 -0300

    feat: sprint 2

--- src/br/edu/pucminas/biblioteca/modelo/Usuario.java
+++ src/br/edu/pucminas/biblioteca/modelo/Usuario.java
@@ -4,4 +4,10 @@ public abstract class Usuario {
 
     private String matricula;
     private String senha;
+
+    // HU01
+    public boolean login(String matricula, String senha) {
+        // TODO: implementar na Sprint 3
+        return false;
+    }
 }
